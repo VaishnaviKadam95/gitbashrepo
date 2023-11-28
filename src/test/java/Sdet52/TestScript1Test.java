cbc1ecf62d5327e433fbc13ebe3576197ffd3327
VaishnaviKadam95 <vaishnavikadam01@gmail.com>
2023-11-28 18:36:38 +0530

    Framework is done

---
+++ src/test/java/Sdet52/TestScript1Test.java
@@ -0,0 +1,19 @@
+package Sdet52;
+
+import org.testng.annotations.Test;
+
+public class TestScript1Test {
+	@Test(groups={"smoke"})
+public void ts1() {
+	System.out.println("==TS1===");
+}
+	@Test(groups={"smoke","regression"})
+public void ts2() {
+	System.out.println("==TS2===");
+}
+	@Test(groups={"smoke"})
+public void ts3() {
+	System.out.println("=TS 3===");
+}
+	
+}
